7fea824fe4c7c7cff4e586ee175c524a35c29099
vibhor-57 <vibhor.bhatia2022a@vitstudent.ac.in>
2024-05-29 22:56:09 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="home">
+        <div className="navbar">
+          <h1>
+              Your Logo
+          </h1>
+          <ul>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+          </ul>
+        </div>
+        <div className="describe">
+          <h1>3D Glass Homomorphism</h1>
+          <h1>Landing Page</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
